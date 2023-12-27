233973f146864201aa2f61f2e0953ea6c8e9fa30
Muizzuddin-github <muizzuddin334@gmail.com>
2023-12-27 09:44:15 +0700

    add handler function in main

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
-package main
+package api
 
 import (
 	"context"
 	"crud/src"
 	"crud/src/db"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 
-func main() {
-	// mongodb connection
+
+func Handler() *fiber.App{
+
 	ctx := context.Background()
 	db.CreateConn(ctx)
 	defer db.CloseDB(ctx)
 
 	server := src.Application()
-	server.Listen(":8080")
+	return server
 }
